6ac9e4c02e661fa9d6fa8ec11430c13162fa1051
rezakazazy8 <rezakazazy8@yahoo.com>
2024-10-15 17:45:31 +0330

    add dbcontext to project

---
+++ DocAppointment.Infrastructure.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace DocAppointment.Infrastructure.Persistence.Context;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    #region Doctor
+
+    public DbSet<Doctor> Doctors { get; set; }
+
+
+    #endregion
+
+
+
+}
+
